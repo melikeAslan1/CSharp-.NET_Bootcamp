359ad44fe62151255595072504c2b8bb62913806
Melike Aslan <1melike2@gmail.com>
2022-03-27 21:08:10 +0300

    calisma.

---
+++ Deger-Referans/Deger-Referans/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deger_Referans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler1 = { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = { "Adana", "Bursa", "Bolu" };
+            sehirler2 = sehirler1;
+
+            Console.WriteLine(sehirler1[0]);
+            Console.WriteLine(sehirler2[1]);
+
+
+
+        }
+    }
+}
